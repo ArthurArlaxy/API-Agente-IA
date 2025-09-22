a618dbfb602b4311565ff6f3f27db717e5b8c105
Arthur Albuquerque <arthuraadeoliveira@gmail.com>
2025-09-22 17:20:21 -0300

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,8 @@
 import express from "express"
+import { AuthController } from "./controller/authController"
+
 
 export const router = express.Router()
+const authController = new AuthController()
 
-router
+router.post("/register", authController.register)
