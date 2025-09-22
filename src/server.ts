8ae3f06a933a3da60383055e39d1ba81831fb810
Arthur Albuquerque <arthuraadeoliveira@gmail.com>
2025-09-22 17:20:24 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express  from "express";
 import { router } from "./routes";
+import { errorHandler } from "./middleware/errorHandlerMiddleware";
+import "dotenv/config";
 
 const app = express()
 
 app.use(express.json())
 
-app.use(router)
+app.use('/api',router)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 
