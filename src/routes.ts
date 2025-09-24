9a2ba8f27f72f30ea9ba95ebed60f962cf025ba7
Arthur Albuquerque <arthuraadeoliveira@gmail.com>
2025-09-23 21:09:18 -0300

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,14 @@
 import express from "express"
 import { AuthController } from "./controller/authController"
+import { AgentController } from "./controller/agentController"
+import { authMiddleware } from "./middleware/authMiddleware"
 
 
 export const router = express.Router()
 const authController = new AuthController()
+const agentController = new AgentController()
 
 router.post("/register", authController.register)
+router.get("/login", authController.login)
+router.get("/home", authMiddleware ,agentController.index)
+router.get("/IaAgent", authMiddleware, agentController.chat)
